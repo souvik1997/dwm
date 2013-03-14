834cd015329380a3a438bc30a37910b348f3629d
Souvik <souvik1997@gmail.com>
2013-03-14 15:56:01 -0500

    Added useless gaps

--- config.h
+++ config.h
@@ -68,6 +68,7 @@ static const Rule rules[] = {
 static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
+static const unsigned int gappx     = 6; // Useless gaps
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
